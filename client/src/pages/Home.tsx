c6d8ae1519b8270a37bde6cde458374ffe27ed69
Jakey Wakey <askjake331@gmail.com>
2022-06-29 20:27:07 -0400

    hide button for quick tools menu

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { VisibilityOffOutlined } from "@mui/icons-material"
 import React, { FC } from "react"
 import { pageTitle } from "../functions/common/pageSettings"
 
@@ -8,14 +9,31 @@ interface HomeProps {
 export const Home: FC<HomeProps> = ({title}) => {
 	pageTitle(title)
 
+	const handleToolHide = () => {
+		console.log('clicked "Hide Tools"')
+		const box = document.getElementById('quick-tools')
+		box!.style.height = '0px'
+		box!.style.padding = '0px'
+		box!.style.opacity = '0'
+		box!.style.overflow = 'hidden'
+		box!.style.marginTop = '0px'
+		box!.style.marginBottom = '-1rem'
+	}
+
+	const iconStyle = {
+		height: '1rem',
+		width: '1rem',
+	}
+
 	return (
 		<div className="home-content">
-			<div className="quick-tools-block">
+			<div className="quick-tools-block" id='quick-tools'>
 				<button>Sell</button>
 				<button>Sell</button>
 				<button>Sell</button>
 				<button>Sell</button>
 				<div className="quick-tools-title">Quick Tool Menu</div>
+				<div className="quick-tools-hide" onClick={handleToolHide}><VisibilityOffOutlined style={iconStyle} /> Hide Tools</div>
 			</div>
 			<div className="category-block">Category Tiles</div>
 			<div className="featured-items-block">Featured Items</div>
